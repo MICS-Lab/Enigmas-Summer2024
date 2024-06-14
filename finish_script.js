f582b8af8083ab0cd40c8be99386a92649dada87
pauldubois98 <pauldubois98@gmail.com>
2024-06-14 09:38:45 +0200

    finish script

---
+++ finish_script.js
@@ -0,0 +1,18 @@
+function final_code() {
+  var password =
+    document.getElementById("password0").value;
+
+  var sha_password = SHA256(
+    password.replace(/ /g, "")
+  );
+  var user_code = sha_password.substring(0, 8).toUpperCase();
+  if (
+    SHA256(user_code) ==
+    "e09620d99f4519a88bdaa7fc247150e59db8329f36025728fb77a904b88efb92"
+  ) {
+    document.getElementById("finish-code").innerHTML =
+      'Finish code: <span id="code">' + user_code + "</span>";
+  } else {
+    document.getElementById("finish-code").innerHTML = "";
+  }
+}
